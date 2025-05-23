ff1834a361294e413a4104b307a37ac90bb9a7de
Adrian-24914 <pen24914@uvg.edu.gt>
2025-05-22 18:36:58 -0600

    Junit para grafos

---
+++ GrafoTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+public class GrafoTest {
+    private Grafo grafo;
+
+    @BeforeEach
+    public void setUp() {
+        grafo = new Grafo();
+    }
+
+    @Test
+    public void testAgregarCiudad() {
+        grafo.agregarCiudad("CiudadX");
+        assertTrue(grafo.getCiudades().contains("CiudadX"));
+        assertEquals(1, grafo.getCiudades().size());
+    }
+
+    @Test
+    public void testAgregarYEliminarArco() {
+        grafo.agregarArco("A", "B", new int[]{5, 10, 15, 20});
+        int[][] matriz = grafo.getMatriz(Clima.NORMAL);
+        int i = grafo.getCiudades().indexOf("A");
+        int j = grafo.getCiudades().indexOf("B");
+        assertEquals(5, matriz[i][j]);
+
+        grafo.eliminarArco("A", "B");
+        assertEquals(Integer.MAX_VALUE / 2, grafo.getMatriz(Clima.NORMAL)[i][j]);
+    }
+}
